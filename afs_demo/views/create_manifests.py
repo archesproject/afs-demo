a83a1d07363b69bbe7036f81a367467350cf1cf7
njkim <nkim@fargeo.com>
2025-06-30 22:50:34 -0700

    Remove unncessary function to create a new one

--- afs_demo/views/create_manifests.py
+++ afs_demo/views/create_manifests.py
@@ -94,41 +94,6 @@ class CreateManifest(View):
             },
         }
 
-    def add_canvases(self, manifest, canvases):
-        manifest.manifest["sequences"][0]["canvases"] += canvases
-
-    def check_canvas_in_use(cself, canvas_id):
-        return VwAnnotation.objects.filter(canvas=canvas_id).exists()
-
-    def delete_canvases(self, manifest, canvases_to_remove):
-        canvas_ids_remove = [
-            canvas["images"][0]["resource"]["service"]["@id"]
-            for canvas in canvases_to_remove
-        ]
-        canvases_in_use = []
-        for canvas_id in canvas_ids_remove:
-            if self.check_canvas_in_use(canvas_id):
-                canvases_in_use.append(canvas_id)
-        canvases = manifest.manifest["sequences"][0]["canvases"]
-        if len(canvases_in_use) > 0:
-            canvas_labels_in_use = [
-                item["label"]
-                for item in canvases
-                if item["images"][0]["resource"]["service"]["@id"]
-                in canvases_in_use
-            ]
-            raise ManifestValidationError(
-                "The following canvases cannot be deleted because they have resource annotations: {}".format(
-                    ", ".join(canvas_labels_in_use)
-                )
-            )
-        manifest.manifest["sequences"][0]["canvases"] = [
-            canvas
-            for canvas in canvases
-            if canvas["images"][0]["resource"]["service"]["@id"]
-            not in canvas_ids_remove
-        ]
-
     def create_image(self, file, scheme, host):
         new_image_id = uuid.uuid4()
         new_image_file = File.objects.create(fileid=new_image_id, path=file)
@@ -142,28 +107,6 @@ class CreateManifest(View):
         image_json = self.fetch(file_json_url)
 
         return image_json, new_image_id, file_url
-
-    def change_manifest_info(self, manifest, name, desc, attribution, logo):
-        if name is not None and name != "":
-            manifest.label = name
-            manifest.manifest["label"] = name
-        if desc is not None and desc != "":
-            manifest.description = desc
-            manifest.manifest["description"] = desc
-        if attribution and attribution != "":
-            manifest.manifest["attribution"] = attribution
-        if logo and logo != "":
-            manifest.manifest["logo"] = logo
-
-    def change_manifest_metadata(self, manifest, metadata):  # To be fixed
-        manifest.manifest["metadata"] = metadata
-
-    def change_canvas_label(self, manifest, canvas_id, label):
-        # canvas_id = canvas['images'][0]['resource']['service']['@id']
-        canvases = manifest.manifest["sequences"][0]["canvases"]
-        for canvas in canvases:
-            if canvas["images"][0]["resource"]["service"]["@id"] == canvas_id:
-                canvas["label"] = label
 
     def post(self, request: HttpRequest):
         acceptable_types = [
@@ -189,7 +132,6 @@ class CreateManifest(View):
         manifest_url = request.POST.get("manifest")
         canvas_label = request.POST.get("canvas_label")
         canvas_id = request.POST.get("canvas_id")
-        metadata = request.POST.get("metadata")
         transaction_id = request.POST.get("transaction_id", uuid.uuid1())
         selected_canvases = request.POST.get("selected_canvases")
         scheme = request.scheme
@@ -199,71 +141,34 @@ class CreateManifest(View):
         except TypeError:
             metadata = []
 
-        if operation == "create":
-            canvases = []
-            for f in files:
-                if os.path.splitext(f.name)[1].lower() in acceptable_types:
-                    image_json, image_id, file_url = self.create_image(f, scheme, host)
+        canvases = []
+        for f in files:
+            if os.path.splitext(f.name)[1].lower() in acceptable_types:
+                image_json, image_id, file_url = self.create_image(f, scheme, host)
 
-                    canvas = self.create_canvas(
-                        image_json, file_url, os.path.splitext(f.name)[0], image_id
-                    )
-                    canvases.append(canvas)
-                else:
-                    logger.warning("filetype unacceptable: " + f.name)
+                canvas = self.create_canvas(
+                    image_json, file_url, os.path.splitext(f.name)[0], image_id
+                )
+                canvases.append(canvas)
+            else:
+                logger.warning("filetype unacceptable: " + f.name)
 
-            pres_dict = self.create_manifest(
-                name=name,
-                canvases=canvases,
-                file_url=canvases[0]["thumbnail"]["service"]["@id"],
-            )
-            manifest_global_id = str(uuid.uuid4())
-            json_url = f"/manifest/{manifest_global_id}"
-            pres_dict["@id"] = f"{request.scheme}://{request.get_host()}{json_url}"
+        pres_dict = self.create_manifest(
+            name=name,
+            canvases=canvases,
+            file_url=canvases[0]["thumbnail"]["service"]["@id"],
+        )
+        manifest_global_id = str(uuid.uuid4())
+        json_url = f"/manifest/{manifest_global_id}"
+        pres_dict["@id"] = f"{request.scheme}://{request.get_host()}{json_url}"
 
-            manifest = IIIFManifest.objects.create(
-                label=name,
-                description=desc,
-                manifest=pres_dict,
-                url=json_url,
-                globalid=manifest_global_id,
-                transactionid=transaction_id,
-            )
+        manifest = IIIFManifest.objects.create(
+            label=name,
+            description=desc,
+            manifest=pres_dict,
+            url=json_url,
+            globalid=manifest_global_id,
+            transactionid=transaction_id,
+        )
 
-            return JSONResponse(manifest)
-        else:
-            manifest = IIIFManifest.objects.get(url=manifest_url)
-
-        self.change_manifest_info(manifest, name, desc, attribution, logo)
-
-        if canvas_label is not None:
-            self.change_canvas_label(manifest, canvas_id, canvas_label)
-
-        if selected_canvases is not None:
-            selected_canvases_json = json.loads(selected_canvases)
-            try:
-                self.delete_canvases(manifest, selected_canvases_json)
-            except ManifestValidationError as e:
-                return JSONResponse({"message": e.message}, status=500)
-
-        if len(files) > 0:
-            try:
-                canvases = []
-                for f in files:
-                    if os.path.splitext(f.name)[1].lower() in acceptable_types:
-                        image_json, image_id, file_url = self.create_image(f, scheme, host)
-                        canvas = self.create_canvas(
-                            image_json, file_url, os.path.splitext(f.name)[0], image_id
-                        )
-                        canvases.append(canvas)
-                    else:
-                        logger.warning("filetype unacceptable: " + f.name)
-                self.add_canvases(manifest, canvases)
-            except:
-                logger.warning("You have to select a manifest to add images")
-                raise
-
-        self.change_manifest_metadata(manifest, manifest)
-
-        manifest.save()
         return JSONResponse(manifest)
