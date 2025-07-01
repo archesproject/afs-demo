760d4f3ea3a286a1e6afed99bb9afe6420107664
njkim <nkim@fargeo.com>
2025-07-01 14:18:35 -0700

    Interate over the tiles to create manifest

--- afs_demo/views/create_manifests.py
+++ afs_demo/views/create_manifests.py
@@ -7,17 +7,25 @@ from django.db import connection
 from django.http import HttpRequest
 from django.utils.translation import gettext as _
 from django.views.generic import View
-from arches.app.models.models import File, IIIFManifest
+from arches.app.models.models import File, IIIFManifest, TileModel
 from arches.app.models.system_settings import settings
 from arches.app.utils.response import JSONResponse
 
 logger = logging.getLogger(__name__)
 
 
-cantaloupe_uri = f"{settings.CANTALOUPE_HTTP_ENDPOINT.rstrip('/')}/iiif"
+ARCHES_URL = settings.PUBLIC_SERVER_ADDRESS.rstrip('/')
+CANTALOUPE_URI = f"{settings.CANTALOUPE_HTTP_ENDPOINT.rstrip('/')}/iiif"
+ACCEPTABLE_TYPES = [
+    ".jpg",
+    ".jpeg",
+    ".tiff",
+    ".tif",
+    ".png",
+]
 
 
-def create_manifest(
+def create_manifest_json(
     name="",
     desc="",
     file_url="file_url",
@@ -26,7 +34,7 @@ def create_manifest(
     canvases=[],
     metadata=[],
 ):
-    sequence_id = f"{cantaloupe_uri}/manifest/sequence/TBD.json"
+    sequence_id = f"{CANTALOUPE_URI}/manifest/sequence/TBD.json"
 
     return {
         "@context": "http://iiif.io/api/presentation/2/context.json",
@@ -55,8 +63,8 @@ def create_manifest(
 
 
 def create_canvas(image_json, file_url, file_name, image_id):
-    canvas_id = f"{cantaloupe_uri}/manifest/canvas/{image_id}.json"
-    image_id = f"{cantaloupe_uri}/manifest/annotation/{image_id}.json"
+    canvas_id = f"{CANTALOUPE_URI}/manifest/canvas/{image_id}.json"
+    image_id = f"{CANTALOUPE_URI}/manifest/annotation/{image_id}.json"
     thumbnail_width = 300 if image_json["width"] >= 300 else image_json["width"]
     thumbnail_height = 300 if image_json["height"] >= 300 else image_json["height"]
     thumbnail_id = (
@@ -112,14 +120,14 @@ def fetch(url):
         raise
 
 
-def create_image(file, scheme, host):
+def create_image(file):
     new_image_id = uuid.uuid4()
     new_image_file = File.objects.create(fileid=new_image_id, path=file)
     new_image_file.save()
 
     file_name = os.path.basename(new_image_file.path.name)
-    file_url = f"{scheme}://{host}/iiifserver/iiif/2/{file_name}"
-    file_json_url = f"{cantaloupe_uri}/2/{file_name}/info.json"
+    file_url = f"{ARCHES_URL}/iiifserver/iiif/2/{file_name}"
+    file_json_url = f"{CANTALOUPE_URI}/2/{file_name}/info.json"
     image_json = fetch(file_json_url)
 
     return image_json, new_image_id, file_url
@@ -159,45 +167,75 @@ def create_manifest_record(name, desc, transaction_id, pres_dict, json_url, mani
     return manifest
 
 
+# this function is the main entry point for creating a manifest service
 def create_manifest_service(
-    files, name, desc, attribution, logo, transaction_id, scheme, host, metadata
+    files, 
+    name="",
+    desc="",
+    attribution="",
+    logo="",
+    transaction_id=None,
+    metadata=[],
 ):
-    acceptable_types = [
-        ".jpg",
-        ".jpeg",
-        ".tiff",
-        ".tif",
-        ".png",
-    ]
+    if not transaction_id:
+        transaction_id = uuid.uuid1()
+    if not name:
+        try:
+            name = os.path.splitext(files[0]["name"])[0]
+        except IndexError:
+            name = _("Untitled Manifest")
 
     canvases = []
     for f in files:
-        if os.path.splitext(f.name)[1].lower() in acceptable_types:
-            image_json, image_id, file_url = create_image(f, scheme, host)
+        image_json, image_id, file_url = get_image_info(f)
 
-            canvas = create_canvas(
-                image_json, file_url, os.path.splitext(f.name)[0], image_id
-            )
-            canvases.append(canvas)
-        else:
-            logger.warning("filetype unacceptable: " + f.name)
+        canvas = create_canvas(
+            image_json, file_url, os.path.splitext(f["name"])[0], image_id
+        )
+        canvases.append(canvas)
 
-    pres_dict = create_manifest(
-        name=name,
-        canvases=canvases,
-        file_url=canvases[0]["thumbnail"]["service"]["@id"],
-        desc=desc,
-        attribution=attribution,
-        logo=logo,
-        metadata=metadata,
-    )
-    manifest_global_id = str(uuid.uuid4())
-    json_url = f"/manifest/{manifest_global_id}"
-    pres_dict["@id"] = f"{scheme}://{host}{json_url}"
+    if canvases:
+        pres_dict = create_manifest_json(
+            name=name,
+            canvases=canvases,
+            file_url=canvases[0]["thumbnail"]["service"]["@id"],
+            desc=desc,
+            attribution=attribution,
+            logo=logo,
+            metadata=metadata,
+        )
+        manifest_global_id = str(uuid.uuid4())
+        json_url = f"/manifest/{manifest_global_id}"
+        pres_dict["@id"] = f"{ARCHES_URL}{json_url}"
 
-    manifest = create_manifest_record(name, desc, transaction_id, pres_dict, json_url, manifest_global_id, True)
+        manifest = create_manifest_record(name, desc, transaction_id, pres_dict, json_url, manifest_global_id, False)
 
     return manifest
+
+
+def get_image_info(file):
+    file_id = file["file_id"]
+    file_name = os.path.basename(file["name"])
+    file_url = f"{ARCHES_URL}/iiifserver/iiif/2/{file_name}"
+    file_json_url = f"{CANTALOUPE_URI}/2/{file_name}/info.json"
+    image_json = fetch(file_json_url)
+
+    return image_json, file_id, file_url
+
+
+def create_manifests_from_tiles():
+    DIGITAL_RESOURCES_NODEGROUPID = "7c486328-d380-11e9-b88e-a4d18cec433a"
+    digital_resource_file_tiles = TileModel.objects.filter(
+        nodegroup_id=DIGITAL_RESOURCES_NODEGROUPID,
+    ).iterator()
+    for tile in digital_resource_file_tiles:
+        for file in tile.data['7c486328-d380-11e9-b88e-a4d18cec433a']:
+            if file["file_id"] is not None and file["file_id"] != "":
+                if os.path.splitext(file["name"])[1].lower() in ACCEPTABLE_TYPES:
+                    create_manifest_service([file])
+                else:
+                    logger.warning("filetype unacceptable: " + file["name"])
+
 
 
 class CreateManifest(View):
@@ -222,7 +260,7 @@ class CreateManifest(View):
             metadata = []
 
         manifest = create_manifest_service(
-            files, name, desc, attribution, logo, transaction_id, scheme, host, metadata
+            files, name, desc, attribution, logo, transaction_id, metadata
         )
 
         return JSONResponse(manifest)
